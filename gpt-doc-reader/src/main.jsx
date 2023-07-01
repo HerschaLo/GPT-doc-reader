7565ab1e465059a3c601d6037b6a3bc3774cd7b9
HerschaLo <stephen.dt.le@gmail.com>
2023-06-30 22:26:56 -0400

    created pages folder

--- gpt-doc-reader/src/main.jsx
+++ gpt-doc-reader/src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div><h1 className="text-3xl font-bold underline">Hello world!</h1></div>,
+    element: <h1 className="text-3xl font-bold underline">Hello world!</h1>,
   },
 ]);
 
