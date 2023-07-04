6a7b106c1d1fa4cb0524f7aae5d892329fd42fbd
Jerry Wu <jerrywu2390@gmail.com>
2023-07-04 09:51:42 -0400

    add testing pdf

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from dotenv import load_dotenv, find_dotenv
 from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from PyPDF2 import PdfReader
+from langchain.embeddings import OpenAIEmbeddings
 load_dotenv(find_dotenv())
-llm = OpenAI(model_name="text-davinci-003")
-print(llm("explain how humans fart"))
+
+
+reader = PdfReader("test.pdf")
+number_of_pages = len(reader.pages)
+page = reader.pages[0]
+text = page.extract_text()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 0,
+)
+res = text_splitter.create_documents([text])
+
+embeddings = OpenAIEmbeddings(model_name='ada')
+query_result = embeddings.embed_query(res[0].page_content)
+
+
 
 
